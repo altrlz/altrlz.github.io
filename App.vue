68ee53912610da69d6b425159d0160de4b87679e
altrlz <alto.realize31@gmail.com>
2019-03-15 00:08:01 +0900

    practice
    I should learn what is vue-cli.

---
+++ App.vue
@@ -0,0 +1,30 @@
+<template>
+    <div id="app">
+        <tab-menu></tab-menu>
+        <router-view/>
+    </div>
+</template>
+
+<script>
+
+import TabMenu from '@/components/TabMenu'
+
+export default {
+    name: 'App',
+    components: {
+        TabMenu
+    }
+}
+</script>
+
+<style>
+#app {
+    font-family: 'Avenir', Helvetica, Aral, sans-serif;
+    -weblit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: graysacale;
+    text-aling: center;
+    color: #2c3e50;
+    margin-top: 60px;
+}
+
+</style>
